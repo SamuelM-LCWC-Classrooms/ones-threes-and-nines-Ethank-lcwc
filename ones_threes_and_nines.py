4243b1c7b5bb5bedef660f80eeaef2239bbf7b0d
Ethank-lcwc <144791953+Ethank-lcwc@users.noreply.github.com>
2024-12-16 13:34:30 +0000

    helllooooo

--- ones_threes_and_nines.py
+++ ones_threes_and_nines.py
@@ -1,1 +1,15 @@
-
+class ones_three_nines:
+    def __init__(self,num:float):
+        self.__num = num
+        self.__ones = num
+        self.__threes = int(num / 3)
+        self.__nines = int(num / 9)
+    
+    def ones(self):
+        return self.__ones
+    
+    def threes(self):
+        return self.__threes
+    
+    def nines(self):
+        return self.__nines
